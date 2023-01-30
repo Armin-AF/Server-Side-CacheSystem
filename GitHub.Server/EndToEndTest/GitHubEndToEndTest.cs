f30da81bc4df143b86f1789c6006f5aebe3ebb0a
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-01-30 15:32:36 +0100

    End2End tests

---
+++ GitHub.Server/EndToEndTest/GitHubEndToEndTest.cs
@@ -0,0 +1,76 @@
+using GitHub.Server.Controllers;
+using GitHub.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace EndToEndTest;
+
+[TestClass]
+public class GitHubEndToEndTest
+{
+    private readonly GitHubController _controller;
+    private readonly IMemoryCache _memoryCache;
+
+
+    public GitHubEndToEndTest()
+    {
+        _memoryCache = new MemoryCache(new MemoryCacheOptions());
+        _controller = new GitHubController(_memoryCache);
+    }
+
+    [TestMethod]
+    public async Task GetUser_ReturnsCachedUser_WhenUsernameExistsInCache()
+    {
+        // Arrange
+        var username = "testuser";
+        var expectedUser = new GitHubUser
+        {
+            Login = username,
+            Id = 12345,
+            AvatarUrl = "https://avatars.com/user.png",
+            HtmlUrl = "https://github.com/user",
+            Url = "https://api.github.com/users/user"
+        };
+        _memoryCache.Set(username, expectedUser, new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+        });
+
+        // Act
+        var result = await _controller.GetUser(username);
+
+        // Assert
+        var actualUser = (GitHubUser)((OkObjectResult)result).Value;
+        Assert.AreEqual(expectedUser, actualUser);
+    }
+
+    [TestMethod]
+    public async Task GetUser_ReturnsUserFromApi_WhenUsernameDoesNotExistInCache()
+    {
+        // Arrange
+        var controller = new GitHubController(new MemoryCache(new MemoryCacheOptions()));
+        var username = "testuser";
+
+        // Act
+        var result = await controller.GetUser(username);
+        var user = (GitHubUser)((ObjectResult)result).Value;
+
+        // Assert
+        Assert.IsNotNull(user);
+        Assert.AreEqual(username, user.Login);
+    }
+
+    [TestMethod]
+    public async Task GetUser_AddsUserToCache_WhenUsernameDoesNotExistInCache()
+    {
+        // Arrange
+        var username = "testuser";
+
+        // Act
+        await _controller.GetUser(username);
+
+        // Assert
+        var user = _memoryCache.Get<GitHubUser>(username);
+        Assert.IsNotNull(user);
+    }
+}
