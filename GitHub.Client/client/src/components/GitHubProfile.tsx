a0c255d4082bacfd6da4ff1b5446a1281da9ca8e
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-02-01 16:54:36 +0100

    Adds Time taken

--- GitHub.Client/client/src/components/GitHubProfile.tsx
+++ GitHub.Client/client/src/components/GitHubProfile.tsx
@@ -21,15 +21,19 @@ const GitHubProfile: React.FC = () => {
     const [user, setUser] = useState<GitHubUser | null>(null);
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState(false);
+    const [timeTaken, setTimeTaken] = useState<number | null>(null);
 
     const fetchData = async (username: string) => {
         setLoading(true);
+        const startTime = Date.now();
         try {
             const response = await axios.get(`https://localhost:7215/api/github/${username}`);
             setUser(response.data);
         } catch (error) {
             setError((error as any).message);
         }
+        const endTime = Date.now();
+        setTimeTaken(endTime - startTime);
         setLoading(false);
     };
 
@@ -71,6 +75,7 @@ const GitHubProfile: React.FC = () => {
                     <p>Number of public repositories: {user.public_repos}</p>
                     <p>Profile URL: <a href={user.html_url}>{user.html_url}</a></p>
                     <p>Is from cache: {user.isFromCache ? 'Yes' : 'No'}</p>
+                    <p>Time taken: {timeTaken}ms</p>
                 </>
             )}
             <button className={"clear-button"} type="button" onClick={handleClear}>Clear</button>
